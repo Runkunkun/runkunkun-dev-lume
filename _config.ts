10f140b9f3a5155895781412d985d867b95fdd5c
Lukas Kurth <lukas@SyngenioPro.fritz.box>
2023-07-07 00:50:15 +0200

    first version of start page

--- _config.ts
+++ _config.ts
@@ -1,6 +1,8 @@
 import lume from "lume/mod.ts";
 import tailwindcss from "lume/plugins/tailwindcss.ts";
 import postcss from "lume/plugins/postcss.ts";
+import imagick from "lume/plugins/imagick.ts";
+import date from "lume/plugins/date.ts";
 
 const site = lume({
     src: "./src",
@@ -8,5 +10,8 @@ const site = lume({
 site.ignore("flake.nix", "flkae.lock");
 site.use(tailwindcss());
 site.use(postcss());
+site.use(imagick());
+site.use(date());
+site.loadAssets([".svg"]);
 
 export default site;
